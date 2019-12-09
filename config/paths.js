695a4f4f3c51d2f9b57ae841b0a71d8f60501540
herisn23 <herisn23@gmail.com>
2019-12-10 00:57:09 +0100

    fixed build

--- config/paths.js
+++ config/paths.js
@@ -69,7 +69,7 @@ const resolveModule = (resolveFn, filePath) => {
 module.exports = {
   dotenv: resolveApp('.env'),
   appPath: resolveApp('.'),
-  appBuild: resolveApp('target/classes/public'),
+  appBuild: resolveApp('build'),
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
   appIndexJs: resolveModule(resolveApp, 'src/index'),
